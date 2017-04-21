69ed356c5589613963736d1e3eb02a21d3668f14
akaxiaok <akaxiaok@gmail.com>
2017-04-21 23:09:57 +0800

    doc(style) fix some lint

--- src/Component/Signin.jsx
+++ src/Component/Signin.jsx
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import { connect } from 'react-redux';
 import action from '../Action/Index';
-import {Tool} from '../Tool';
-import {Header} from './common/index';
-import {browserHistory} from 'react-router'
+import { Tool } from '../Tool';
+import { Header } from './common/index';
 /**
  * 模块入口
  *
@@ -11,53 +12,48 @@ import {browserHistory} from 'react-router'
  * @extends {Component}
  */
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            button: '登录'
-        };
-        this.signin = () => {
-            var accesstoken = this.refs.accesstoken.value;
-            if (!accesstoken) return alert('不能为空！');
-            this.setState({button: '登录中...'});
-            Tool.post('/api/v1/accesstoken', {accesstoken}, (res) => {
-                if (res.success) {
-                    alert('登录成功');
-                    res.accesstoken = accesstoken;
-                    this.props.signinSuccess(res);
-                    browserHistory.push({
-                        pathname: '/user/' + res.loginname
-                    });
-                } else {
-                    alert('登录失败');
-                    this.setState({button: '登录'});
-                }
-
-            }, () => {
-                alert('登录失败！');
-                this.setState({button: '登录'});
-            });
+  constructor(props) {
+    super(props);
+    this.state = {
+      button: '登录',
+    };
+    this.signin = () => {
+      const accesstoken = this.accesstoken.value;
+      if (!accesstoken) return alert('不能为空！');
+      this.setState({ button: '登录中...' });
+      Tool.post('/api/v1/accesstoken', { accesstoken }, (res) => {
+        if (res.success) {
+          alert('登录成功');
+          res.accesstoken = accesstoken;
+          this.props.signinSuccess(res);
+          browserHistory.push({
+            pathname: `/user/${res.loginname}`,
+          });
+        } else {
+          alert('登录失败');
+          this.setState({ button: '登录' });
         }
+      }, () => {
+        alert('登录失败！');
+        this.setState({ button: '登录' });
+      });
+    };
+  }
 
-    }
-
-    render() {
-        return (
-            <div>
-                <Header title="登录" leftIcon="fanhui"/>
-                <div className="signin" data-flex="dir:top main:center cross:center">
-                    <div className="center">
-                        <div className="text"><input ref="accesstoken" type="text" placeholder="Access Token"/></div>
-                        <button className="btn" onClick={this.signin}>{this.state.button}</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header title="登录" leftIcon="fanhui" />
+        <div className="signin" data-flex="dir:top main:center cross:center">
+          <div className="center">
+            <div className="text"><input ref={(accesstoken) => { this.accesstoken = accesstoken; }} type="text" placeholder="Access Token" /></div>
+            <button className="btn" onClick={this.signin}>{this.state.button}</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 
-
-export default connect((state) => {
-    return {User: state.User};
-}, action('User'))(Main); //连接redux
+export default connect(state => ({ User: state.User }), action('User'))(Main); // 连接redux
