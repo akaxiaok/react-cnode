578ee20eaa234d7a452a4773d4f9abc867d1d767
akaxiaok <akaxiaok@gmail.com>
2017-05-01 17:49:29 +0800

    refactor(action) delete _ID

--- src/Action/Action.jsx
+++ src/Action/Action.jsx
@@ -1,14 +1,13 @@
-export default (_ID) => {
+export default () => {
   const action = {};
   const arr = [
     'signinSuccess', // 登录成功
     'signin', // 退出登录
     'setState', // 设置状态
   ];
-
   for (let i = 0; i < arr.length; i++) {
-    action[arr[i]] = target => ({ _ID, target, type: arr[i] });
+    // action[arr[i]] = target => ({ _ID, target, type: arr[i] });
+    action[arr[i]] = target => ({ target, type: arr[i] });
   }
-
   return action;
 };
