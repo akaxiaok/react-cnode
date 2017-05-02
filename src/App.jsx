28bd748a15e2ba7fec52fe9310c944ba3c2e388b
akaxiaok <akaxiaok@gmail.com>
2017-05-02 22:41:54 +0800

    refactor(*) fix some lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import  {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import route from './Config/Route'; //路由配置
+import 'normalize.css'; // 重置浏览器默认样式
+import 'flex.css'; // flex布局
+import 'github-markdown-css'; // markdown css
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import route from './Config/Route'; // 路由配置
 import store from './Config/Store';
+import './Style/style.less'; // 加载公共样式
+import './Iconfont/iconfont.css'; // 字体图标
 
-
-import 'normalize.css'; //重置浏览器默认样式
-import 'flex.css'; //flex布局
-import './Style/style.less'; //加载公共样式
-import './Iconfont/iconfont.css'; //字体图标
-import 'github-markdown-css'; //markdown css
-
-store.subscribe(function () {
+store.subscribe(() => {
     // console.log(store.getState());
 });
 
 
 render(
-    <Provider store={store}>
-        {route}
-    </Provider>,
-    document.body.appendChild(document.createElement('div'))
+  <Provider store={store}>
+    {route}
+  </Provider>,
+    document.body.appendChild(document.createElement('div')),
 );
