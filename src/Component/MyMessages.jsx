f401b5f0e00b3fa2fed758563139d65dd3d84a4b
akaxiaok <akaxiaok@gmail.com>
2017-06-16 23:24:34 +0800

    refactor(*) merger MyMessage & GetData

--- src/Component/MyMessages.jsx
+++ src/Component/MyMessages.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import GetData from './GetData2';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import TipMsgSignIn from './TipMsgSignIn';
 import Content from './Content';
 import DataLoad from './DataLoad';
 import Footer from './Footer';
 import Header from './Header';
+import action from '../Action/Action';
+
+const setting = {
+  id: 'Messages',  // 应用关联使用的redux
+  url: '/api/v1/messages', // 服务器请求的地址
+  stop: props => !props.data, // true 拦截请求，false不拦截请求
+  data: props =>  // 发送给服务器的数据
+    ({ accesstoken: props.user.accesstoken }),
+};
 
 /**
  * 模块入口
@@ -14,20 +24,50 @@ import Header from './Header';
  * @extends {Component}
  */
 class Main extends Component {
-  render() {
-    const { data, loadAnimation, loadMsg } = this.props.state;
-    const { User } = this.props;
-    let main = null;
-    if (!User) {
-      main = <TipMsgSignIn />;
-    } else if (!data) {
-      main = <DataLoad loadAnimation={loadAnimation} loadMsg={loadMsg} />;
-    } else {
-      const { hasnot_read_messages, has_read_messages } = data;
-      Array.prototype.push.apply(hasnot_read_messages, has_read_messages);
-      main = <Content list={hasnot_read_messages} />;
+  constructor(props) {
+    super(props);
+    if (!props.user) {
+      browserHistory.push('/');
     }
+    this.redayDOM = () => {
+      if (this.get) return false; // 已经加载过
+      // window.scrollTo(scrollX, scrollY); // 设置滚动条位置
+      if (this.testStop()) return false; // 请求被拦截
+      const { mdrender, accesstoken } = setting.data(this.props, this.props.status);
+      this.props.getMessage({
+        url: setting.url,
+        mdrender,
+        accesstoken,
+      });
+      return true;
+    };
+    this.testStop = () => {
+      const { stop } = this.props.seting;
+      if (typeof stop === 'function') {
+        return stop(this.props);
+      }
+      return stop;
+    };
+  }
 
+  componentDidMount() {
+    this.redayDOM();
+  }
+
+  render() {
+    const { pages, status } = this.props.data;
+    const { loadAnimation, loadMsg } = status;
+    const { user } = this.props;
+    let main = null;
+    if (!user) {
+      main = <TipMsgSignIn />;
+    } else if (!loadAnimation) {
+      const { hasnot_read_messages, has_read_messages } = pages;
+      Array.prototype.push.apply(hasnot_read_messages, has_read_messages);
+      main = <Content list={ hasnot_read_messages} />;
+    } else {
+      main = <DataLoad loadAnimation={loadAnimation} loadMsg={loadMsg} />;
+    }
     return (
       <div>
         <Header title="消息" />
@@ -37,13 +77,8 @@ class Main extends Component {
     );
   }
 }
-export default GetData2({
-  id: 'MyMessages',  // 应用关联使用的redux
-  component: Main, // 接收数据的组件入口
-  url: '/api/v1/messages', // 服务器请求的地址
-  stop: (props, state) => !props.User, // true 拦截请求，false不拦截请求
-  data: (props, state) =>  // 发送给服务器的数据
-         ({ accesstoken: props.User.accesstoken }),
-  success: state => state, // 请求成功后执行的方法
-  error: state => state, // 请求失败后执行的方法
-});
+Main.defaultProps = { setting };
+
+export default connect(state =>
+  ({ data: state[setting.id], user: state.User }), action(setting.id))(Main); // 连接redux
+
