12317ed5985e4445968492bea8877ab89ba374fe
akaxiaok <akaxiaok@gmail.com>
2017-06-20 00:15:32 +0800

    refactor(UserView) merger UserView & GetData

--- src/Component/UserView.jsx
+++ src/Component/UserView.jsx
@@ -1,37 +1,65 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import GetData2 from './GetData2';
+import { browserHistory } from 'react-router';
+import { connect } from 'react-redux';
 import DataLoad from './DataLoad';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import action from '../Action/Action';
 
-/**
- * 模块入口
- *
- * @class Main
- * @extends {Component}
- */
+
+const setting = {
+  id: 'UserView',  // 应用关联使用的redux
+  url: props => `/api/v1/user/${props.params.loginname}`,
+  stop: props => props.data,
+};
+
+
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = this.props.state;
-    this.tab = (tabIndex) => {
-      this.state.tabIndex = tabIndex;
-      this.props.setState(this.state);
+    this.switchTab = (tabIndex) => {
+      this.props.switchTab(tabIndex);
+    };
+
+    if (!props.user) {
+      browserHistory.push('/');
+    }
+    this.redayDOM = () => {
+      // if (this.get) return false; // 已经加载过
+      debugger
+      if (this.testStop()) return false; // 请求被拦截
+      this.props.getUserView({
+        url: this.props.setting.url(this.props),
+      });
+      return true;
+    };
+    this.testStop = () => {
+      const { stop } = this.props.setting;
+      if (typeof stop === 'function') {
+        return stop(this.props);
+      }
+      return stop;
     };
   }
 
+  componentDidMount() {
+    this.redayDOM();
+  }
+
+
   render() {
-    const { data, loadAnimation, loadMsg, tabIndex } = this.props.state;
-    let { User, params } = this.props;
-    User = User || {};
-    const main = data ? <Home data={data} tabIndex={tabIndex} tab={this.tab} /> :
+    const { loadAnimation, loadMsg, tabIndex } = this.props.status;
+    const data = this.props.data;
+    let { user, params } = this.props;
+    user = user || {};
+    const main = data ? <Home data={data} tabIndex={tabIndex} tab={this.switchTab} /> :
       <DataLoad loadAnimation={loadAnimation} loadMsg={loadMsg} />;
-    const title = params.loginname === User.loginname ? '个人中心' : `${params.loginname}的个人中心`;
-    const footer = params.loginname === User.loginname ? <Footer index="3" /> : null;
-    const leftIcon = params.loginname === User.loginname ? null : 'fanhui';
-    const rightIcon = params.loginname === User.loginname ? 'tuichu' : null;
+    const title = params.loginname === user.loginname ? '个人中心' : `${params.loginname}的个人中心`;
+    const footer = params.loginname === user.loginname ? <Footer index="3" /> : null;
+    const leftIcon = params.loginname === user.loginname ? null : 'fanhui';
+    const rightIcon = params.loginname === user.loginname ? 'tuichu' : null;
     return (
       <div>
         <Header title={title} leftIcon={leftIcon} rightIcon={rightIcon} rightTo="/signout" />
@@ -42,12 +70,8 @@ class Main extends Component {
   }
 
 }
+Main.defaultProps = { setting };
 
-export default GetData2({
-  id: 'UserView',  // 应用关联使用的redux
-  component: Main, // 接收数据的组件入口
-  url: (props, state) => `/api/v1/user/${props.params.loginname}`,
-  data: {},
-  success: state => state, // 请求成功后执行的方法
-  error: state => state, // 请求失败后执行的方法
-});
+export default connect(state =>
+    ({ status: state[setting.id].status, data: state[setting.id].data, user: state.User }),
+  action(setting.id))(Main); // 连接redux
