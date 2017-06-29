9b7fe7e74c8a21ce73fec04f925b47f23f709301
akaxiaok <akaxiaok@gmail.com>
2017-06-29 21:24:00 +0800

    refactor(ReplyBox) change to use the method provide by parent component to reply

--- src/Component/ReplyBox.jsx
+++ src/Component/ReplyBox.jsx
@@ -24,33 +24,23 @@ export default class ReplyBox extends Component {
       this.state = { btnname: '提交中...' };
       const data = this.props.data;
       if (data.reply_id) {
-        data.content = `[@${this.props.loginname}](/user/${this.props.loginname}) ${this.refs.content.value}`;
+        data.content = `[@${this.props.loginname}](/user/${this.props.loginname}) ${this.content.value}`;
       } else {
-        data.content = this.refs.content.value;
+        data.content = this.content.value;
       }
-      if (data.content == '') {
+      if (!data.content) {
         return alert('回复内容不能为空！');
       }
-      data.content += '\n\r<br><br>来自<a href="https://lzxb.github.io/react-cnode/" target="_blank">react-cnode手机版</a>';
-      Tool.post(`/api/v1//topic/${data.id}/replies`, data, (res) => {
-        this.setState({ btnname: '回复成功，刷新页面中..' });
-        this.refs.content.value = '';
-        Tool.get(`/api/v1//topic/${data.id}`, {}, (res) => {
-          this.props.reLoadData(res.data); // 刷新页面
-          this.setState({ btnname: '回复' });
-        }, () => {
-          this.state = { btnname: '刷新失败，请手动刷新试试' };
-        });
-      }, (res) => {
-        this.setState({ btnname: '回复失败' });
-      });
+      data.content += '-----from------';
+      this.props.reLoadData(data);
+      return true;
     };
   }
 
   render() {
     return (
       <div className="reply-box" style={{ display: this.props.display }} >
-        <div className="text" ><textarea ref="content" placeholder={this.props.placeholder} /></div>
+        <div className="text" ><textarea ref={ref => (this.content = ref)} placeholder={this.props.placeholder} /></div>
         <div data-flex="main:right" >
           <button className="btn" onClick={this.submit} >{this.state.btnname}</button>
         </div>
