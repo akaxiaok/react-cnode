a31dd14e88d81e355429f308cb106bb83a9c8509
akaxiaok <akaxiaok@gmail.com>
2017-07-18 21:57:42 +0800

    refactor(ReplyBox) style

--- src/Component/ReplyBox.jsx
+++ src/Component/ReplyBox.jsx
@@ -11,47 +11,47 @@ import { Tool } from '../Tool';
  * @extends {Component}
  */
 export default class ReplyBox extends Component {
+  static defaultProps = {
+    display: 'block',
+    placeholder: '回复支持Markdown语法,请注意标记代码',
+  };
+
   constructor(props) {
     super(props);
     this.state = { btnname: '回复' };
 
-    /**
-     * 提交回复
-     *
-     * @returns
-     */
-    this.submit = () => {
-      // this.state = { btnname: '提交中...' };
-      const data = this.props.data;
-      if (data.reply_id) {
-        data.content = `[@${this.props.loginname}](/user/${this.props.loginname}) ${this.content.value}`;
-      } else {
-        data.content = this.content.value;
-      }
-      if (!data.content) {
-        return alert('回复内容不能为空！');
-      }
-      data.content += '' +
-        '-----from------';
-      this.props.replyTopic(data);
-      this.content.value = "";
-      return true;
-    };
   }
+
+  /**
+   * 提交回复
+   *
+   * @returns
+   */
+  submit = () => {
+    // this.state = { btnname: '提交中...' };
+    const data = this.props.data;
+    if (data.reply_id) {
+      data.content = `[@${this.props.loginname}](/user/${this.props.loginname}) ${this.content.value}`;
+    } else {
+      data.content = this.content.value;
+    }
+    if (!data.content) {
+      return alert('回复内容不能为空！');
+    }
+    data.content += '' +
+      '-----from------';
+    this.props.replyTopic(data);
+    this.content.value = "";
+    return true;
+  };
 
   render() {
     return (
       <div className="reply-box" style={{ display: this.props.display }} >
         <div className="text" ><textarea ref={ref => (this.content = ref)} placeholder={this.props.placeholder} /></div>
-        <div data-flex="main:right" >
-          <button className="btn" onClick={this.submit} >{this.state.btnname}</button>
-        </div>
+        <button className="btn" onClick={this.submit} >{this.state.btnname}</button>
       </div>
     );
   }
 }
 
-ReplyBox.defaultProps = {
-  display: 'block',
-  placeholder: '回复支持Markdown语法,请注意标记代码',
-};
