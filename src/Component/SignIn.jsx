39e0132fa75594418b17f526379fcc627c135734
akaxiaok <akaxiaok@gmail.com>
2017-07-20 21:29:58 +0800

    refactor(SignIn): use material-ui

--- src/Component/SignIn.jsx
+++ src/Component/SignIn.jsx
@@ -2,6 +2,9 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Snackbar from 'material-ui/Snackbar';
 import action from '../Action/Action';
 import { Tool } from '../Tool';
 import Header from './Header';
@@ -15,43 +18,63 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      open: false,
+      message: '',
+      value: '',
       button: '登录',
     };
-    this.signin = () => {
-      const accesstoken = this.accesstoken.value;
-      if (!accesstoken) return alert('不能为空！');
-      this.setState({ button: '登录中...' });
-      Tool.post('/api/v1/accesstoken', { accesstoken }, (res) => {
-        if (res.success) {
-          alert('登录成功');
-          res.accesstoken = accesstoken;
-          this.props.signinSuccess(res);
-          browserHistory.push({
-            pathname: `/user/${res.loginname}`,
-          });
-        } else {
-          alert('登录失败');
-          this.setState({ button: '登录' });
-        }
-      }, () => {
-        alert('登录失败！');
-        this.setState({ button: '登录' });
-      });
-    };
+
+  }
+
+  signin = () => {
+    const accesstoken = this.state.value;
+    if ('' === accesstoken) {
+      this.setState({ message: '请输入Access Token', open: true });
+      return;
+    }
+    this.setState({ button: '登录中...' });
+    Tool.post('/api/v1/accesstoken', { accesstoken }, (res) => {
+      if (res.success) {
+        res.accesstoken = accesstoken;
+        this.props.signinSuccess(res);
+        browserHistory.push({
+          pathname: `/user/${res.loginname}`,
+        });
+      } else {
+        this.setState({ button: '登录', message: '登录失败', open: true });
+      }
+    }, () => {
+      this.setState({ button: '登录', message: '登录失败', open: true });
+    });
+  };
+  handleChange = (event, newValue) => {
+    this.setState({ value: newValue });
+  }
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  }
+  handleActionTouchTap = () => {
+    this.setState({ open: false });
   }
 
   render() {
     return (
       <div>
         <Header title="登录" leftIcon="back" />
-        <div className="signin" data-flex="dir:top main:center cross:center" >
+        <div className="signin flex justify-content-center align-items-center" >
           <div className="center" >
-            <div className="text" ><input ref={(accesstoken) => {
-              this.accesstoken = accesstoken;
-            }} type="text" placeholder="Access Token" /></div>
-            <button className="btn" onClick={this.signin} >{this.state.button}</button>
+            <TextField onChange={this.handleChange} fullWidth={true} hintText={'Access Token'} />
+            <RaisedButton fullWidth={true} onTouchTap={this.signin} primary={true} label={this.state.button} />
           </div>
         </div>
+        <Snackbar
+          open={this.state.open}
+          message={this.state.message}
+          action="OK"
+          autoHideDuration={3000}
+          onRequestClose={this.handleRequestClose}
+          onActionTouchTap={this.handleActionTouchTap}
+        />
       </div>
     );
   }
